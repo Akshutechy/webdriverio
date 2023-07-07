8729a1f09490e06fcbcb68f6d09677df697196e2
Akshutechy <akshaya.techy@gmail.com>
2023-07-07 22:24:47 +0530

    first commit

---
+++ test/Data/Data.js
@@ -0,0 +1,21 @@
+module.exports = {
+    validUserName:'standard_user',
+    validPassword:'secret_sauce',
+    invalidUserName:'Akshaya',
+    invalidPassword:'Narayanan',
+
+    errorMessage:{
+        emptyLogin:'Username is required',
+        invalidLogin:'Username and password do not match any user in this service'
+    },
+
+    checkoutStepone:{
+        firstName:'Akshaya',
+        lastName: 'Narayanan',
+        Zip: '625017'
+    }
+
+    // addToCart:{
+    //     oneProduct:''
+    // }
+}
